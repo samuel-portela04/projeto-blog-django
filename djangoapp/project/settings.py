2d1406bac1aa364c4e254b00d0ed5dc2acbc0dff
Samuel Portela <samucaportela1@gmail.com>
2024-08-06 14:27:33 -0300

    Atualizações do dia 06/08/2024

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'site_setup',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'site_setup.context_processors.context_processor_example',
+                'site_setup.context_processors.site_setup',
             ],
         },
     },
---
+++ djangoapp/site_setup/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from site_setup.models import MenuLink, SiteSetup
+
+
+class MenuLinkInline(admin.TabularInline):
+    model = MenuLink
+    extra = 1
+
+
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+    inlines = MenuLinkInline,
+
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.exists()
