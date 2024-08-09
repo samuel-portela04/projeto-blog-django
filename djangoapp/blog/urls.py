4f974fb08d2b36bbd771504164f3aeb7a7b31509
Samuel Portela <samucaportela1@gmail.com>
2024-08-09 11:55:45 -0300

    Atualização ListView e DetailView

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,15 +1,14 @@
-from blog.views import category, created_by, index, page, post, tag, search
+from blog.views import (PostListView, CategoryListView, CreatedByListView, PageDetailView, PostDetailView, SearchListView, TagListView)
 from django.urls import path
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('post/', post, name='post'),
-    path('post/<slug:slug>/', post, name='post'),
-    path('page/<slug:slug>/', page, name='page'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
-    path('category/<slug:slug>/', category, name='category'),
-    path('tag/<slug:slug>/', tag, name='tag'),
-    path('search/', search, name='search'),
+    path('', PostListView.as_view(), name='index'),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post'),
+    path('page/<slug:slug>/', PageDetailView.as_view(), name='page'),
+    path('created_by/<int:author_pk>/', CreatedByListView.as_view(), name='created_by'),
+    path('category/<slug:slug>/', CategoryListView.as_view(), name='category'),
+    path('tag/<slug:slug>/', TagListView.as_view(), name='tag'),
+    path('search/', SearchListView.as_view(), name='search'),
 ]
